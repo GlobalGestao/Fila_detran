fa24f6aea3315b5c447654f6edf7ac4183f95aa4
Clinica Global <gestao@cgbtran.com.br>
2024-09-24 17:03:19 -0300

    Update script_fila.js

--- PANIEL_SENHA/static/scripts/script_fila.js
+++ PANIEL_SENHA/static/scripts/script_fila.js
@@ -20,7 +20,7 @@ async function adicionarPaciente(tipo) {
 
     // Enviar dados para a API
     try {
-        const response = await fetch('http://127.0.0.1:5000/adicionar-paciente', {
+        const response = await fetch('http://127.0.0.1:8000/adicionar-paciente', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',

6432c12de047da26c182172c4d8f320c8269855f
Clinica Global <gestao@cgbtran.com.br>
2024-09-24 17:05:34 -0300

    Update script_tv.js

--- PANIEL_SENHA/static/scripts/script_tv.js
+++ PANIEL_SENHA/static/scripts/script_tv.js
@@ -5,7 +5,7 @@ const maxTentativas = 3;
 
 // Função para atualizar o atendimento atual
 function atualizarAtendimentoAtual() {
-    fetch('http://127.0.0.1:5000/get-atendimento-atual')
+    fetch('http://127.0.0.1:8000/get-atendimento-atual')
         .then(response => response.json())
         .then(atendimentoAtual => {
             const attendanceSection = document.getElementById('current-attendance');

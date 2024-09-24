fbb7223d5e8081e6a792b8b3a5a3f971444c6892
Clinica Global <gestao@cgbtran.com.br>
2024-09-24 15:24:16 -0300

    Update app.py
    app mudado

--- PANIEL_SENHA/BackEnd/app.py
+++ PANIEL_SENHA/BackEnd/app.py
@@ -3,7 +3,8 @@ from flask_cors import CORS
 from flask_socketio import SocketIO
 import psycopg2
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates')
+
 CORS(app, resources={r"/*": {"origins": "*"}})  # Ajuste de CORS
 socketio = SocketIO(app)
 
